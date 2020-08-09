aa9fbeed184f04eecb2c5a54a48604ba9756e12d
bean1310 <22887835+bean1310@users.noreply.github.com>
2020-08-09 19:01:27 +0900

    Implemented ModVersionManager and Test

--- src/ModVersionManager.py
+++ src/ModVersionManager.py
@@ -3,7 +3,6 @@
 
 import json
 import os.path
-import sys
 
 
 class Singleton(object):
@@ -16,6 +15,7 @@ class Singleton(object):
             cls._instance.input = input
         return cls._instance
 
+
 class ModVersionManagerError(Exception):
     """This Error is Base Error of ModVersionManager
     """
@@ -23,26 +23,27 @@ class ModVersionManagerError(Exception):
     def __init__(self, message):
         self.message = message
 
+
 class DataBaseAccessError(ModVersionManagerError):
     """Raised when failed to access database file
     """
-    
+
     pass
+
 
 class ModNotFoundError(ModVersionManagerError):
     """Raised when mod don't find in database
     """
     pass
 
+
 class BadMethodCallError(ModVersionManagerError):
     """Raised when wrong call method
     """
     pass
 
-class ModVersionManager(Singleton):
 
-    __modsList = {}
-    __dataFilePath = ""
+class ModVersionManager(Singleton):
 
     def __init__(self, dataFilePath):
         """constructor
@@ -53,15 +54,19 @@ class ModVersionManager(Singleton):
         Raises:
             DataBaseAccessError: Raised when failed to open database file
         """
+        self.__modsList = {}
         self.__dataFilePath = dataFilePath
         try:
-            if os.path.isfile(self.__dataFilePath) == False:
+            if os.path.isfile(self.__dataFilePath) is False:
                 with open(file=self.__dataFilePath, mode='w') as dataFile:
                     json.dump(self.__modsList, dataFile)
             with open(file=self.__dataFilePath, mode='r') as dataFile:
                 self.__modsList = json.load(dataFile)
-        except EnvironmentError:
+        except OSError:
             raise DataBaseAccessError('Failed to open ' + dataFilePath + '\n')
+
+    def ___del__(self):
+        self.flush()
 
     def register(self, name, version):
         """Register Mod info
@@ -71,11 +76,13 @@ class ModVersionManager(Singleton):
             version (string): Mod version
 
         Raises:
-            BadMethodCallError: Raised when calling this method despite mod is already register.
+            BadMethodCallError: Raised when calling this method \
+                despite mod is already register.
         """
         if name in self.__modsList:
-            raise BadMethodCallError(name + " is already registerd. You should use update method.")
-        
+            raise BadMethodCallError(
+                name + " is already registerd. You should use update method.")
+
         self.__modsList[name] = version
 
     def update(self, name, version):
@@ -94,9 +101,28 @@ class ModVersionManager(Singleton):
         self.__modsList[name] = version
 
     def getModNameList(self):
-        return self.__modsList.keys()
+        """Return mod name list
+
+        Returns:
+            list: All mod name
+        """
+        return list(self.__modsList.keys())
 
     def getModVersion(self, name):
+        """Return mod version
+
+        Args:
+            name (string): Mod name
+
+        Raises:
+            ModNotFoundError: Raise when the given mod name was not registered.
+
+        Returns:
+            string: Mod version
+        """
+        if name not in self.__modsList:
+            raise ModNotFoundError(name + "is not found in database")
+
         return self.__modsList[name]
 
     def delete(self, name):
@@ -126,5 +152,5 @@ class ModVersionManager(Singleton):
         try:
             with open(file=self.__dataFilePath, mode='w') as dataFile:
                 json.dump(self.__modsList, dataFile)
-        except IOError:
+        except (OSError):
             raise DataBaseAccessError("Failed to persistent")
--- tests/testModVersionManager.py
+++ tests/testModVersionManager.py
@@ -6,7 +6,9 @@ import shutil
 import os
 from os.path import join, dirname
 
-from src.ModVersionManager import ModVersionManager
+from src.ModVersionManager \
+    import ModVersionManager, ModNotFoundError, BadMethodCallError
+
 
 class TestModVersionManager(unittest.TestCase):
 
@@ -16,21 +18,80 @@ class TestModVersionManager(unittest.TestCase):
         self.__testOutPutFilePath = join(self.__testOutputDir, 'data')
 
     def tearDown(self):
+        del self.__mvm
         shutil.rmtree(self.__testOutputDir)
 
     def testSetAndGet(self):
         modName = "hoge"
         modVersion = "1.12.0"
 
-        modVersionManager = ModVersionManager(self.__testOutPutFilePath)
+        self.__mvm = ModVersionManager(self.__testOutPutFilePath)
 
-        modVersionManager.register(modName, modVersion)
+        self.__mvm.register(modName, modVersion)
 
-        modNameList = modVersionManager.getModNameList()
-        self.assertTrue(modName in modNameList)
-        
-        self.assertEqual(modVersion, modVersionManager.getModVersion(modName))
+        self.assertIn(modName, self.__mvm.getModNameList())
+
+        self.assertEqual(modVersion, self.__mvm.getModVersion(modName))
+
+    def testStoreAndLoad(self):
+        modName = "foo"
+        modVersion = "1.12.1"
+
+        self.__mvm = ModVersionManager.getInstance(self.__testOutPutFilePath)
+
+        self.__mvm.register(modName, modVersion)
+        self.__mvm.flush()
+
+        del self.__mvm
+
+        self.__mvm = ModVersionManager.getInstance(self.__testOutPutFilePath)
+
+        self.assertEqual(self.__mvm.getModVersion(modName), modVersion)
+
+    def testModNotFoundError(self):
+        modName = "neko"
+        modVersion = "2.9.9"
+
+        self.__mvm = ModVersionManager.getInstance(self.__testOutPutFilePath)
+        self.__mvm.register(modName, modVersion)
+
+        with self.assertRaises(ModNotFoundError):
+            self.__mvm.getModVersion("DontRegisterdModName")
+
+        with self.assertRaises(ModNotFoundError):
+            self.__mvm.update('DontRegisterdModName', '10.0')
+
+        with self.assertRaises(ModNotFoundError):
+            self.__mvm.delete('DontRegisterdModName')
+
+    def testBadMethodCallError(self):
+        modName = "hoge"
+        modVersion = "1.0"
+
+        self.__mvm = ModVersionManager.getInstance(self.__testOutPutFilePath)
+
+        self.__mvm.register(modName, modVersion)
+
+        newVersion = "1.1"
+
+        with self.assertRaises(BadMethodCallError):
+            self.__mvm.register(modName, newVersion)
+
+    # def testDataBaseAccessError(self):
+    #     modName = "innu"
+    #     modVersion = "1.0"
+    #     testDataFilePath = self.__testOutPutFilePath
+
+    #     self.__mvm = ModVersionManager.getInstance(testDataFilePath)
+
+    #     self.__mvm.register(modName, modVersion)
+    #     self.__mvm.flush()
+
+    #     os.remove(testDataFilePath)
+
+    #     with self.assertRaises(DataBaseAccessError):
+    #         self.__mvm.flush()
+
 
 if __name__ == "__main__":
     unittest.main()
-    
